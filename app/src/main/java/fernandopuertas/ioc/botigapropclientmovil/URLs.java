011e07a7e99027ca28c7569a5149c39af0daf15e
Beth Isart <elisabet_isart@hotmail.com>
2020-12-14 00:31:22 +0100

    fix: fix connection errors

--- app/src/main/java/fernandopuertas/ioc/botigapropclientmovil/URLs.java
+++ app/src/main/java/fernandopuertas/ioc/botigapropclientmovil/URLs.java
@@ -2,7 +2,7 @@ package fernandopuertas.ioc.botigapropclientmovil;
 
 public class URLs {
 
-    private static final String ROOT_URL = "http://localhost:8080";
+    private static final String ROOT_URL = "http://10.0.2.2:8080/";
 
     public static final String URL_REGISTER = ROOT_URL + "/registre";
     public static final String URL_LOGIN= ROOT_URL + "/login";
